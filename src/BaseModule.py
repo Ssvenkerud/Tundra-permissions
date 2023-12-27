62a8df79c1d1735d4779bb4af210dadf4579966d
Simen Svenkerud <s.svenkerud@gmail.com>
2023-12-27 14:59:03 +0100

    Base module and permission_entity

---
+++ src/BaseModule.py
@@ -0,0 +1,27 @@
+from .Entity import Permission_entity
+
+class BaseModule:
+    def __init__(self, type):
+        self.type = type
+        self.entitiees = {}
+
+    def add_entity(self, entity):
+        if not isinstance(entity, Permission_entity):
+            raise ValueError(f'Invalid entity type: {type(entity)}')
+
+        self.entitiees[entity.name] = entity
+
+    def get_entity(self, name):
+        return self.entitiees[name]
+
+    def get_entities(self):
+        return self.entitiees
+
+    def get_type(self):
+        return self.type
+
+    def __repr__(self):
+        return f'BaseModule(type={self.type}, entitiees={self.entitiees})'
+
+
+
---
+++ src/Entity.py
@@ -0,0 +1,6 @@
+class Permission_entity():
+
+    def __init__(self, name, data, permision_type="udefined"):
+        self.name = name
+        self.type = permision_type
+        self.data = data
